90add46bb6c3e29ea75647858851b6e4b1ebb96d
PatrichBravo <patrich.bravo@gmail.com>
2023-10-01 13:02:57 +1300

    Crear interfaz grafica basica

--- cli.py
+++ cli.py
@@ -1,15 +1,8 @@
-def get_todos(filepath='todos.txt'):
-    """ Read a text file and return the list of to-do items."""
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(todos_arg, filepath='todos.txt'):
-    """ Write the to-do items list in trhe txt file
-    w is for Write in text file"""
-    with open(filepath,'w') as file:
-        file.writelines(todos_arg)
-    
+#from functions import get_todos, write_todos
+import functions
+import time
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("Today is",now)
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -19,18 +12,18 @@ while True:
     if user_action.startswith ('add'):
         todo = user_action[4:]
         
-        todos = get_todos()
+        todos = functions.get_todos()
         
         todos.append(todo + "\n")
         
-        write_todos(todos)
+        functions.write_todos(todos)
         
 
             
 
     elif user_action.startswith('show'):
         
-        todos = get_todos()
+        todos = functions.get_todos()
             
         
         for index, item in enumerate(todos):
@@ -43,12 +36,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
             
-            todos = get_todos()
+            todos = functions.get_todos()
             
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
             
-            write_todos(todos)
+            functions.write_todos(todos)
             
 
         except ValueError:
@@ -58,14 +51,14 @@ while True:
     elif user_action.startswith('complete'):
         try:
             number = int(user_action[9:])
-            todos = get_todos('todos.txt')
+            todos = functions.get_todos('todos.txt')
                 
             index = number - 1
             todo_to_remove = todos[index].strip("\n")
             
             todos.pop(index)
             
-            write_todos(todos)
+            functions.write_todos(todos)
               
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
