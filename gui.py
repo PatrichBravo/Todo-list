52f36e5a4dd68718ef380ced7b1a0a814f8febe6
PatrichBravo <patrich.bravo@gmail.com>
2023-10-03 00:15:13 +1300

    make add button work

--- gui.py
+++ gui.py
@@ -1,10 +1,24 @@
-#import functions
+import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-imput_box = sg.InputText (tooltip="Enter todo")
+input_box = sg.InputText (tooltip="Enter todo", key="todo")
 add_botton = sg.Button ("Add")
-windows = sg.Window('My To-do App', layout=[[label], [imput_box,add_botton]])
+windows = sg.Window('My To-do App',
+                    layout=[[label], [input_box,add_botton]],
+                    font=('helvetica' , 20))
 
-windows.read()
+while True:
+    event, values = windows.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+    
 windows.close() 
